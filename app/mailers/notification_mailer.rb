d4420764a24f2373080776ae6e903e3fa50db2c0
Ali Thompson <alilynne.thompson@gmail.com>
2017-05-31 21:24:19 +0000

    Adjusted mailer

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,8 +1,10 @@
 class NotificationMailer < ApplicationMailer
   default from: "no-reply@handstandapp.com"
 
-  def comment_added
-    mail(to: "alifirehose@gmail.com",
-      subject: "A comment has been added to your place")
+  def comment_added(comment)
+    @place = comment.place
+    @place_owner = @place.user
+    mail(to: @place_owner.email,
+      subject: "A comment has been added to #{@place.name}")
   end
 end
